791914715f5cdcc829dfc77b5c6cdb4a7a7a1d11
ukukukuk <109439768+ukukukuk@users.noreply.github.com>
2022-07-17 17:24:22 +0300

    function.py

--- function.py
+++ function.py
@@ -4,13 +4,13 @@ print('*' * 10, 'Крестики нолики', '*'*10)
 board = list(range(1,10))
 
 
-def drao_boad(board):
+def draw_board(board):
     print('-'*13)
     for i in range(3):
         print('|', board[0+i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
         print('-'*13) 
 
-drao_boad(board)
+draw_board(board)
 =======
 def position(digit):
     x = digit // 10 #число обозначающее номер столбца
