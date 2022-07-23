f03d9204b47090d845bab28f35fb6c6888296ffc
Haydn4250 <purtsakin.kolia2015@gmail.com>
2022-07-23 20:25:21 +0300

    update function

--- function.py
+++ function.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-print('*' * 10, 'Крестики нолики', '*'*10)
-
-board = list(range(1,10))
-
-
-def draw_board(board):
+def draw_board():
+    board = list(range(1,10))
     print('-'*13)
     for i in range(3):
         print('|', board[0+i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
         print('-'*13) 
 
-draw_board(board)
-=======
 def position(digit):
     x = digit // 10 #число обозначающее номер столбца
     y = digit % 10 #число обозначающее номер строки
@@ -20,4 +13,36 @@ def position(digit):
             if x == i and y == j:
                 return()   #возвращает название переменной в которую вставляется крестик или нолик
                 
->>>>>>> 4449bd48e101e20e53a986a605ab59af7437b0fd
+def take_input(player_token):# ввод пользователя
+   board = list(range(1,10))
+   valid = False
+   while not valid:
+      player_answer = input("Куда поставим " + player_token+"? ")# консоль спрашивает пользователя куда поставить х или y
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Некорректный ввод. Вы уверены, что ввели число?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:# диапозон ввода
+         if(str(board[player_answer-1]) not in "XO"):# проверка что ввёл пользователь
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Некорректный ввод. Введите число от 1 до 9.")
+
+def main(board):
+    board = list(range(1,10))
+    counter = 0
+    res = False
+    while not res:
+        draw_board(board)
+        if counter % 2 == 0:
+           take_input("X")
+        else:
+           take_input("O")
+        counter += 1
+        
+    draw_board(board)
+main(board)
--- tic-tac-toe_game.py
+++ tic-tac-toe_game.py
@@ -1,3 +1,5 @@
+from function import draw_board
+
 print('Добро пожаловать в игру крестики - нолики')
 print('Введите "X", если будете играть крестиком и "0"(ноль), если будете играть ноликом: ', end='')
 player1 = input() #первый игрок
@@ -9,50 +11,3 @@ else:
 print('Поехали!')
 
 #отсюда нужно написать клеточную систему
-
-board = list(range(1,10)) #доска сколько в ней позицый(сколько полей)
-
-
-def draw_board(board): # схема доски из чего она состоит
-    print('-'*13)
-    for i in range(3):
-        print('|', board[0+i*3], '|', board[1+i*3], '|', board[2+i*3], '|')# из чего состоит строчка в консоле ну что разделяет их
-        print('-'*13) 
-
-
-
-
-
-
-def take_input(player_token):# ввод пользователя
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")# консоль спрашивает пользователя куда поставить х или y
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
-         continue
-      if player_answer >= 1 and player_answer <= 9:# диапозон ввода
-         if(str(board[player_answer-1]) not in "XO"):# проверка что ввёл пользователь
-            board[player_answer-1] = player_token
-            valid = True
-         else:
-            print("Эта клетка уже занята!")
-      else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
-
-
-def main(board):
-    counter = 0
-    res = False
-    while not res:
-        draw_board(board)
-        if counter % 2 == 0:
-           take_input("X")
-        else:
-           take_input("O")
-        counter += 1
-        
-    draw_board(board)
-main(board)
